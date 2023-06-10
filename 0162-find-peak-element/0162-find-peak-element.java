3da4043dedacaf4dc09985347d37b9873f14fe8c
kartik <patelkartik18112000@gmail.com>
2023-06-10 08:33:43 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (59.16%) - LeetHub

---
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int low = 0;
+        int high = nums.length -1;
+        if(nums.length == 1){
+            return 0;
+        }
+        if(nums[0] > nums[1]){
+            return 0;
+        }
+        if(nums[nums.length-1] > nums[nums.length-2]){
+            return nums.length-1;
+        }
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(nums[mid] > nums[mid+1] && nums[mid] > nums[mid-1]){
+                return mid;
+            }
+            else if(nums[mid+1] > nums[mid]){
+                low = mid+1;
+            }
+            else if(nums[mid-1] > nums[mid]){
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
