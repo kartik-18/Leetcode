807ebfeedb2085e4639679546a951b259f3577d4
kartik <patelkartik18112000@gmail.com>
2023-06-12 09:40:31 +0530

    Time: 0 ms (100.00%), Space: 41.5 MB (82.49%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,12 +1,18 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
         
-        // NAIVE SOLUTION
+      // TWO POINTER APPROACH
+
+       int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
         
-        for(int i = m, j = 0; j<n ; j++){
-            nums1[i] = nums2[j];
-            i+=1;
+        while (j >= 0) {
+            if (i >= 0 && nums1[i] > nums2[j]) {
+                nums1[k--] = nums1[i--];
+            } else {
+                nums1[k--] = nums2[j--];
+            }
         }
-        Arrays.sort(nums1);
     }
 }
