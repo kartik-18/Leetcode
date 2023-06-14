ca30a1b96ae4ac717f843a3fbe19a860b8aa3627
kartik <patelkartik18112000@gmail.com>
2023-06-15 01:24:45 +0530

    Time: 7 ms (76.84%), Space: 43.8 MB (61.76%) - LeetHub

---
+++ 0463-island-perimeter/0463-island-perimeter.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int islandPerimeter(int[][] grid) {
+        int res = 0;
+        for(int i = 0 ; i < grid.length ;i++){
+            for(int j = 0 ; j < grid[i].length ; j++){
+                if(grid[i][j] == 1){
+                    res +=4;
+                    if(j - 1 >= 0 && grid[i][j-1] ==1){
+                    res--;
+                    }
+                    if(j +1 != grid[i].length && grid[i][j+1] ==1){
+                        res--;
+                    }   
+                    if(i -1 >= 0 && grid[i-1][j] ==1){
+                        res--;
+                    }
+                    if(i+1 != grid.length && grid[i+1][j] ==1){
+                        res--;
+                    }
+                   
+            }
+        }
+        }
+        return res;
+    }
+}
