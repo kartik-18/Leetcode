a17968dc85e8d72eef47fd3462cdb82df71b0d81
kartik <patelkartik18112000@gmail.com>
2023-06-23 11:12:19 +0530

    Time: 2 ms (91.54%), Space: 40.5 MB (78.50%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,22 @@
+class Solution {
+    
+    public boolean matching(char a, char b){
+        return(a == '(' && b == ')' || a == '[' && b == ']' || a == '{' && b == '}');
+    }
+    
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for(int i = 0; i<s.length() ;i++){
+            if(s.charAt(i) == '(' || s.charAt(i) == '[' || s.charAt(i) == '{'){
+                stack.push(s.charAt(i));
+            }else{
+                if(stack.isEmpty() == true){return false;}
+                else if((matching(stack.peek() , s.charAt(i)) == false)){return false;}
+                else{
+                    stack.pop();
+                }
+            }
+        }
+        return (stack.isEmpty() == true);
+    }
+}
