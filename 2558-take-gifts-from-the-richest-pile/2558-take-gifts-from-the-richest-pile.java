c338e86c27659245aa1ea5f030386cc7fbb916b2
kartik <patelkartik18112000@gmail.com>
2023-06-30 22:11:54 +0530

    Time: 34 ms (15.90%), Space: 44.1 MB (5.20%) - LeetHub

---
+++ 2558-take-gifts-from-the-richest-pile/2558-take-gifts-from-the-richest-pile.java
@@ -0,0 +1,15 @@
+class Solution {
+    public long pickGifts(int[] gifts, int k) {
+        int n = gifts.length;
+        for(int i =0 ; i<k ;i++){
+            Arrays.sort(gifts);
+            gifts[n-1] = (int) Math.sqrt(gifts[n-1]);
+        }
+        long sum = 0;
+        
+        for(int i = 0 ; i < gifts.length ;i++){
+            sum += gifts[i];
+        }
+        return sum;
+    }
+}
