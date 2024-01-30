9529ce9d60d84d2f40bc1a70291bbbfa09e2099e
kartik <patelkartik18112000@gmail.com>
2024-01-30 15:03:22 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (25.09%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -12,7 +12,16 @@ class Solution {
         if(nums.length == 1)return nums[0];
         if(nums.length == 2)return Math.max(nums[0],nums[1]);
         int []dp = new int[nums.length];
-        Arrays.fill(dp,-1);
-        return f(nums.length-1,nums,dp);
+        // Arrays.fill(dp,-1);
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0],nums[1]);
+        for(int i = 2 ; i < nums.length; i++){
+            int tk = nums[i]+dp[i-2];
+            int ntk = dp[i-1];
+            
+            dp[i] = Math.max(tk,ntk);
+        }
+        // return f(nums.length-1,nums,dp);
+        return dp[nums.length-1];
     }
 }
